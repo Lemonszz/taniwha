11463174e00c830a6b2de89e5e7ed60cbcb05e0f
Sam <lemonverse@gmail.com>
2022-02-26 15:08:51 +1300

    Check access

--- common/src/main/java/party/lemons/taniwha/mixin/sign/WoodTypeMixin.java
+++ common/src/main/java/party/lemons/taniwha/mixin/sign/WoodTypeMixin.java
@@ -3,7 +3,6 @@ package party.lemons.taniwha.mixin.sign;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import party.lemons.taniwha.hooks.sign.SignTypeHooks;
 
 @Mixin(WoodType.class)
