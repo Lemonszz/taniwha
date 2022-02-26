6137e547b7d274ff7a254ce823502291b1631723
Sam <lemonverse@gmail.com>
2022-02-26 13:37:44 +1300

    blockitempair

---
+++ common/src/main/java/party/lemons/taniwha/util/BlockItemPair.java
@@ -0,0 +1,22 @@
+package party.lemons.taniwha.util;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public record BlockItemPair(Block block, Item item) {
+    public static BlockItemPair of(Block block, Item item) {
+        return new BlockItemPair(block, item);
+    }
+
+    public static BlockItemPair of(Item item, Block block) {
+        return of(block, item);
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+}
