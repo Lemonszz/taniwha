f639f18893618087e6a341d4fbfc18bd223b2347
Sam <lemonverse@gmail.com>
2022-02-27 16:48:04 +1300

    Sign fixes and im not happy about it

--- common/src/main/java/party/lemons/taniwha/hooks/sign/SignTypeHooks.java
+++ common/src/main/java/party/lemons/taniwha/hooks/sign/SignTypeHooks.java
@@ -2,12 +2,35 @@ package party.lemons.taniwha.hooks.sign;
 
 import net.minecraft.world.level.block.state.properties.WoodType;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
 public interface SignTypeHooks
 {
-    static WoodType registerSign(String name)
+    Set<WoodType> bm_getTypes();
+
+    static WoodType register(String name)
     {
-        return ((SignTypeHooks)WoodType.OAK).registerType(name);
+        WoodType type = createWoodType(name);
+
+        ((SignTypeHooks) type).bm_getTypes().add(type);
+
+        return type;
     }
 
-    WoodType registerType(String name);
+    static WoodType createWoodType(String name)
+    {
+        try {
+            Constructor<WoodType> constructor = WoodType.class.getDeclaredConstructor(String.class);
+            constructor.setAccessible(true);
+
+            WoodType type = constructor.newInstance(name);
+            return type;
+        }
+        catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- common/src/main/java/party/lemons/taniwha/mixin/sign/WoodTypeMixin.java
+++ common/src/main/java/party/lemons/taniwha/mixin/sign/WoodTypeMixin.java
@@ -1,22 +1,21 @@
 package party.lemons.taniwha.mixin.sign;
 
 import net.minecraft.world.level.block.state.properties.WoodType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import party.lemons.taniwha.hooks.sign.SignTypeHooks;
 
+import java.util.Set;
+
 @Mixin(WoodType.class)
 public class WoodTypeMixin implements SignTypeHooks {
 
-    @Shadow
-    private static WoodType register(WoodType woodType){
-        return null;
-    }
+    @Shadow @Final
+    private static Set<WoodType> VALUES;
 
     @Override
-    public WoodType registerType(String name)
-    {
-        WoodType type = new WoodType(name);
-        return register(type);
+    public Set<WoodType> bm_getTypes() {
+        return VALUES;
     }
 }
