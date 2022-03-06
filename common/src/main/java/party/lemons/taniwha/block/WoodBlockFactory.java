d299b98c6941037732eeb3acc0106eb50f2d2bf7
Sam <lemonverse@gmail.com>
2022-03-06 18:54:53 +1300

    Fix Wood factory

--- common/src/main/java/party/lemons/taniwha/block/WoodBlockFactory.java
+++ common/src/main/java/party/lemons/taniwha/block/WoodBlockFactory.java
@@ -3,7 +3,7 @@ package party.lemons.taniwha.block;
 import com.google.common.collect.Maps;
 import dev.architectury.registry.block.BlockProperties;
 import dev.architectury.registry.registries.DeferredRegister;
-import net.minecraft.core.Registry;
+import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -12,9 +12,9 @@ import net.minecraft.world.item.SignItem;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.material.Material;
+import org.apache.commons.compress.utils.Lists;
 import party.lemons.taniwha.block.modifier.FlammableModifier;
 import party.lemons.taniwha.block.modifier.RTypeModifier;
 import party.lemons.taniwha.block.modifier.StrippableModifier;
@@ -26,12 +26,16 @@ import party.lemons.taniwha.hooks.sign.SignTypeHooks;
 import party.lemons.taniwha.item.types.TBoatItem;
 import party.lemons.taniwha.registry.BlockWithItem;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class WoodBlockFactory
 {
+    private final List<WoodBlockFactory.Type> types = Lists.newArrayList();
+    private final List<WoodBlockFactory.Type> itemTypes = Lists.newArrayList();
+
     private final Map<Type, Supplier<Block>> blocks = Maps.newHashMap();
     private final Map<Type, Supplier<Item>> items = Maps.newHashMap();
     private final WoodType woodType;
@@ -40,6 +44,7 @@ public class WoodBlockFactory
     private final Consumer<Supplier<Block>> callback;
     private final String modid;
     private final CreativeModeTab tab;
+    private Supplier<BoatType> boatType;
 
     public WoodBlockFactory(String modid, CreativeModeTab group, String name, Block.Properties settings)
     {
@@ -55,84 +60,83 @@ public class WoodBlockFactory
         this.tab = group;
 
         woodType = SignTypeHooks.createWoodType(name);
-
-        set(Type.LOG, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD, new StrippableModifier(()->getBlock(Type.STRIPPED_LOG).get())));
-        set(Type.STRIPPED_LOG, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD));;
-        set(Type.PLANK, ()->new TBlock(settings).modifiers(FlammableModifier.WOOD));
+        types.add(Type.LOG);
+        types.add(Type.STRIPPED_LOG);
+        types.add(Type.PLANK);
     }
 
     public WoodBlockFactory slab()
     {
-        set(Type.SLAB, ()->new TSlabBlock(settings).modifiers(FlammableModifier.WOOD));
+        types.add(Type.SLAB);
         return this;
     }
 
     public WoodBlockFactory stair()
     {
-        set(Type.STAIR, ()->new TStairBlock(getBlock(Type.PLANK).get().defaultBlockState(), settings).modifiers(FlammableModifier.WOOD));
+        types.add(Type.STAIR);
         return this;
     }
 
     public WoodBlockFactory fence()
     {
-        set(Type.FENCE, ()->new TFenceBlock(settings).modifiers(FlammableModifier.WOOD));
-        set(Type.FENCE_GATE, ()->new TFenceGateBlock(settings).modifiers(FlammableModifier.WOOD));
+        types.add(Type.FENCE);
+        types.add(Type.FENCE_GATE);
+
         return this;
     }
 
     public WoodBlockFactory wood()
     {
-        set(Type.WOOD, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD, new StrippableModifier(()->getBlock(Type.STRIPPED_WOOD).get())));
-        set(Type.STRIPPED_WOOD, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD));
+        types.add(Type.WOOD);
+        types.add(Type.STRIPPED_WOOD);
 
         return this;
     }
 
     public WoodBlockFactory pressure_plate()
     {
-        set(Type.PRESSURE_PLATE, ()->new TPressurePlateBlock(PressurePlateBlock.Sensitivity.EVERYTHING, BlockProperties.copy(settings).noCollission()));
+        types.add(Type.PRESSURE_PLATE);
         return this;
     }
 
     public WoodBlockFactory button()
     {
-        set(Type.BUTTON, ()->new TButtonBlock(BlockProperties.copy(settings).noCollission()));
+        types.add(Type.BUTTON);
         return this;
     }
 
     public WoodBlockFactory trapdoor()
     {
-        set(Type.TRAP_DOOR, ()->new TTrapdoorBlock(BlockProperties.copy(settings).noOcclusion()).modifiers(RTypeModifier.create(RType.CUTOUT)));
+        types.add(Type.TRAP_DOOR);
         return this;
     }
 
     public WoodBlockFactory door()
     {
-        set(Type.DOOR, ()->new TDoorBlock(BlockProperties.copy(settings).noOcclusion()).modifiers(RTypeModifier.create(RType.CUTOUT)));
+        types.add(Type.DOOR);
         return this;
     }
 
     public WoodBlockFactory sign()
     {
-        SignBlock standing = new StandingSignBlock(BlockProperties.of(Material.WOOD).strength(1F).sound(SoundType.WOOD).noCollission(), woodType);
-        WallSignBlock wall = new WallSignBlock(BlockProperties.of(Material.WOOD).strength(1F).sound(SoundType.WOOD).noCollission(), woodType);
-        set(Type.SIGN, ()->standing);
-        set(Type.SIGN_WALL, ()->wall);
-        setItem(Type.SIGN, ()->new SignItem(properties(), standing, wall));
-        BlockEntityHooks.addAdditionalBlock(BlockEntityType.SIGN, standing, wall);
+        types.add(Type.SIGN);
+        types.add(Type.SIGN_WALL);
 
+        itemTypes.add(Type.SIGN);
+
+        return this;
+    }
+
+    public WoodBlockFactory boat(Supplier<BoatType> boatType)
+    {
+        this.boatType = boatType;
+        itemTypes.add(Type.BOAT);
         return this;
     }
 
     public Item.Properties properties()
     {
         return new Item.Properties().tab(this.tab);
-    }
-
-    public WoodBlockFactory boat(Supplier<BoatType> boatType)
-    {
-        setItem(Type.BOAT, ()->new TBoatItem(boatType, properties().stacksTo(1)));
-        return this;
     }
 
     public WoodBlockFactory all(Supplier<BoatType> boatType)
@@ -160,39 +164,74 @@ public class WoodBlockFactory
         return items.get(type);
     }
 
-    public WoodBlockFactory register()
+    public WoodBlockFactory register(DeferredRegister<Block> blockRegister, DeferredRegister<Item> itemRegister)
     {
-        DeferredRegister<Block> bR = DeferredRegister.create(modid, Registry.BLOCK_REGISTRY);
-        DeferredRegister<Item> iR = DeferredRegister.create(modid, Registry.ITEM_REGISTRY);
+        for(Type type : types)
+        {
+
+            //TODO: move this to the Type enum
+            switch (type)
+            {
+                case LOG -> set(Type.LOG, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD, new StrippableModifier(()->getBlock(Type.STRIPPED_LOG).get())));
+                case WOOD -> set(Type.WOOD, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD, new StrippableModifier(()->getBlock(Type.STRIPPED_WOOD).get())));
+                case PLANK -> set(Type.PLANK, ()->new TBlock(settings).modifiers(FlammableModifier.WOOD));
+                case STRIPPED_LOG -> set(Type.STRIPPED_LOG, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD));
+                case STRIPPED_WOOD -> set(Type.STRIPPED_WOOD, ()->new TPillarBlock(settings).modifiers(FlammableModifier.WOOD));
+                case SLAB -> set(Type.SLAB, ()->new TSlabBlock(settings).modifiers(FlammableModifier.WOOD));
+                case STAIR -> set(Type.STAIR, ()->new TStairBlock(getBlock(Type.PLANK).get().defaultBlockState(), settings).modifiers(FlammableModifier.WOOD));
+                case FENCE -> set(Type.FENCE, ()->new TFenceBlock(settings).modifiers(FlammableModifier.WOOD));
+                case FENCE_GATE -> set(Type.FENCE_GATE, ()->new TFenceGateBlock(settings).modifiers(FlammableModifier.WOOD));
+                case PRESSURE_PLATE -> set(Type.PRESSURE_PLATE, ()->new TPressurePlateBlock(PressurePlateBlock.Sensitivity.EVERYTHING, BlockProperties.copy(settings).noCollission()));
+                case BUTTON -> set(Type.BUTTON, ()->new TButtonBlock(BlockProperties.copy(settings).noCollission()));
+                case TRAP_DOOR -> set(Type.TRAP_DOOR, ()->new TTrapdoorBlock(BlockProperties.copy(settings).noOcclusion()).modifiers(RTypeModifier.create(RType.CUTOUT)));
+                case DOOR -> set(Type.DOOR, ()->new TDoorBlock(BlockProperties.copy(settings).noOcclusion()).modifiers(RTypeModifier.create(RType.CUTOUT)));
+                case SIGN -> set(Type.SIGN, ()->new StandingSignBlock(BlockProperties.of(Material.WOOD).strength(1F).sound(SoundType.WOOD).noCollission(), woodType));
+                case SIGN_WALL -> set(Type.SIGN_WALL, ()-> new WallSignBlock(BlockProperties.of(Material.WOOD).strength(1F).sound(SoundType.WOOD).noCollission(), woodType));
+            }
+        }
+
+        for(Type type : itemTypes)
+        {
+            switch (type)
+            {
+                case SIGN -> setItem(Type.SIGN, ()->new SignItem(properties(), getBlock(Type.SIGN).get(), getBlock(Type.SIGN_WALL).get()));
+                case BOAT -> setItem(Type.BOAT, ()->new TBoatItem(boatType, properties().stacksTo(1)));
+            }
+        }
 
         for(Type key : blocks.keySet())
         {
             Supplier<Block> bl = blocks.get(key);
-            bR.register(key.make(modid, name), bl);
 
+            ResourceLocation id = key.make(this.modid, name);
+
+            RegistrySupplier<Block> regBlock = blockRegister.register(id, bl);
             if(key.hasBlockItem)
-                if(bl instanceof BlockWithItem bwi)
+            {
+                if(regBlock.get() instanceof BlockWithItem bwi)
                 {
-                    iR.register(key.make(modid, name), ()->bwi.makeItem(tab));
+                    itemRegister.register(key.make(modid, name), ()->bwi.makeItem(tab));
                 }
                 else
                 {
-                    iR.register(key.make(modid, name), ()->new BlockItem(bl.get(), properties()));
+                    itemRegister.register(key.make(modid, name), ()->new BlockItem(regBlock.get(), properties()));
                 }
+            }
 
             if(callback != null)
             {
-                callback.accept(bl);
+                callback.accept(regBlock);
             }
         }
 
         for(Type key : items.keySet())
         {
-            iR.register(key.make(modid, name), items.get(key));
+            itemRegister.register(key.make(modid, name), items.get(key));
         }
 
-        bR.register();
-        iR.register();
+        if(types.contains(Type.SIGN))
+            BlockEntityHooks.addAdditionalBlock(BlockEntityType.SIGN, blocks.get(Type.SIGN).get(), blocks.get(Type.SIGN_WALL).get());
+
         return this;
     }
 
@@ -232,6 +271,7 @@ public class WoodBlockFactory
             this.isItem = isItem;
             this.hasBlockItem = hasBlockItem;
         }
+
 
         public ResourceLocation make(String modid, String name)
         {
