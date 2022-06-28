076f8399e6e852c1726e13bf791acbde98032a7a
Sam <lemonverse@gmail.com>
2022-06-28 19:41:33 +1200

    Switch colour suppliers

--- common/src/main/java/party/lemons/taniwha/client/color/ColorProviderHelper.java
+++ common/src/main/java/party/lemons/taniwha/client/color/ColorProviderHelper.java
@@ -4,9 +4,11 @@ import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.world.level.block.Block;
 
+import java.util.function.Supplier;
+
 public final class ColorProviderHelper
 {
-    public static void registerSimpleBlockWithItem(BlockColor colorProvider, Block... blocks)
+    public static void registerSimpleBlockWithItem(BlockColor colorProvider, Supplier<Block>... blocks)
     {
         ColorHandlerRegistry.registerBlockColors(colorProvider, blocks);
         ColorHandlerRegistry.registerItemColors(new BlockItemColorProvider(), blocks);
