82780cacbf2e3c655c3a39aa377e3ae5d0d4ac53
Sam <lemonverse@gmail.com>
2022-07-07 17:56:04 +1200

    Fix server crash

--- common/src/main/java/party/lemons/taniwha/TaniwhaClient.java
+++ common/src/main/java/party/lemons/taniwha/TaniwhaClient.java
@@ -1,11 +1,21 @@
 package party.lemons.taniwha;
 
+import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import party.lemons.taniwha.entity.TEntities;
+import party.lemons.taniwha.entity.boat.BoatTypes;
+import party.lemons.taniwha.entity.boat.TBoatRender;
 
 public class TaniwhaClient
 {
     public static void init()
     {
-        TEntities.registerModels();
+        registerModels();
+    }
+
+    public static void registerModels()
+    {
+        BoatTypes.registerModelLayers();
+        EntityRendererRegistry.register(TEntities.T_BOAT, c->new TBoatRender(c, false));
+        EntityRendererRegistry.register(TEntities.T_CHEST_BOAT, c->new TBoatRender(c, true));
     }
 }
--- common/src/main/java/party/lemons/taniwha/entity/TEntities.java
+++ common/src/main/java/party/lemons/taniwha/entity/TEntities.java
@@ -1,24 +1,17 @@
 package party.lemons.taniwha.entity;
 
-import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
-import net.minecraft.SharedConstants;
 import net.minecraft.core.Registry;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import party.lemons.taniwha.TConstants;
 import party.lemons.taniwha.Taniwha;
-import party.lemons.taniwha.entity.boat.BoatTypes;
 import party.lemons.taniwha.entity.boat.TBoat;
-import party.lemons.taniwha.entity.boat.TBoatRender;
 import party.lemons.taniwha.entity.boat.TChestBoat;
 
 import java.util.function.Supplier;
 
 public class TEntities {
-    static boolean dfCache = SharedConstants.CHECK_DATA_FIXER_SCHEMA;
-    static {SharedConstants.CHECK_DATA_FIXER_SCHEMA = false;}   //STFU
-
     private static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(TConstants.MOD_ID, Registry.ENTITY_TYPE_REGISTRY);
 
     public static final Supplier<EntityType<? extends TBoat>> T_BOAT = ENTITIES.register(Taniwha.id("t_boat"), ()->
@@ -28,18 +21,8 @@ public class TEntities {
             EntityType.Builder.of((EntityType.EntityFactory<TChestBoat>)TChestBoat::new, MobCategory.MISC).sized(1.375F, 0.5625F).clientTrackingRange(128).updateInterval(3).build("t_chest_boat")
     );
 
-
-    static {SharedConstants.CHECK_DATA_FIXER_SCHEMA = dfCache;}
-
     public static void init()
     {
         ENTITIES.register();
-    }
-
-    public static void registerModels()
-    {
-        BoatTypes.registerModelLayers();
-        EntityRendererRegistry.register(T_BOAT, c->new TBoatRender(c, false));
-        EntityRendererRegistry.register(T_CHEST_BOAT, c->new TBoatRender(c, true));
     }
 }
