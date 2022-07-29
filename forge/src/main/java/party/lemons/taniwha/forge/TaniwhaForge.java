5d4d17ce200ecb06341c027cee9ad01bc78ecb0f
Sam <lemonverse@gmail.com>
2022-07-29 16:05:39 +1200

    Ayo we 1.19.1 now

--- forge/src/main/java/party/lemons/taniwha/forge/TaniwhaForge.java
+++ forge/src/main/java/party/lemons/taniwha/forge/TaniwhaForge.java
@@ -3,8 +3,6 @@ package party.lemons.taniwha.forge;
 import dev.architectury.platform.Platform;
 import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.utils.Env;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,9 +18,6 @@ public class TaniwhaForge {
         EventBuses.registerModEventBus(TConstants.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Taniwha.init();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(TaniwhaForge::clientSetup);
-    }
-
-    private static void modelLoad(ModelRegistryEvent event) {
     }
 
     public static void clientSetup(FMLClientSetupEvent event)
