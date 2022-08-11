51f8dfa7fe1c403c5f297c18de67151badcdb07d
Sam <lemonverse@gmail.com>
2022-08-11 18:19:00 +1200

    Drop experience block

---
+++ common/src/main/java/party/lemons/taniwha/block/types/TDropExperienceBlock.java
@@ -0,0 +1,36 @@
+package party.lemons.taniwha.block.types;
+
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import org.jetbrains.annotations.Nullable;
+import party.lemons.taniwha.block.modifier.BlockModifier;
+import party.lemons.taniwha.block.modifier.BlockWithModifiers;
+import party.lemons.taniwha.registry.BlockWithItem;
+import party.lemons.taniwha.registry.ModifierContainer;
+
+public class TDropExperienceBlock extends DropExperienceBlock implements BlockWithItem, BlockWithModifiers<TDropExperienceBlock>
+{
+	private ModifierContainer<Block> modifierContainer;
+
+	public TDropExperienceBlock(Properties properties)
+	{
+		super(properties);
+	}
+
+	public TDropExperienceBlock(Properties properties, IntProvider intProvider)
+	{
+		super(properties, intProvider);
+	}
+
+	@Override
+	public TDropExperienceBlock modifiers(BlockModifier... modifiers) {
+		modifierContainer = new ModifierContainer<>(this, modifiers);
+		return this;
+	}
+
+	@Override
+	public @Nullable ModifierContainer<Block> getModifierContainer() {
+		return modifierContainer;
+	}
+}
