05639c5200ec9691131ecd6a46a65822675eb510
Sam <lemonverse@gmail.com>
2022-12-09 19:15:57 +1300

    Cache registered items

--- common/src/main/java/party/lemons/taniwha/item/ItemHelper.java
+++ common/src/main/java/party/lemons/taniwha/item/ItemHelper.java
@@ -1,15 +1,22 @@
 package party.lemons.taniwha.item;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import party.lemons.taniwha.item.modifier.ItemWithModifiers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class ItemHelper {
+
+    private static final Map<String, List<RegistrySupplier<Item>>> modItems = Maps.newHashMap();
 
     public static final Consumer<Item> MODIFIER_CONSUMER = (i)->{
         if(i instanceof ItemWithModifiers<?> modifierItem)
@@ -18,11 +25,30 @@ public class ItemHelper {
         }
     };
 
+    public static List<RegistrySupplier<Item>> getItems(String modid)
+    {
+        return modItems.getOrDefault(modid, Lists.newArrayList());
+    }
+
     public static RegistrySupplier<Item> registerItem(DeferredRegister<Item> register, ResourceLocation location, Supplier<Item> itemSupplier)
     {
         RegistrySupplier<Item> registered = register.register(location, itemSupplier);
         registered.listen(MODIFIER_CONSUMER);
 
+        cacheItem(location, registered);
+
         return registered;
+    }
+
+    private static void cacheItem(ResourceLocation location, RegistrySupplier<Item> registered)
+    {
+        List<RegistrySupplier<Item>> items;
+        if(modItems.containsKey(location.getNamespace()))
+            items = modItems.get(location.getNamespace());
+        else {
+            items = new ArrayList<>();
+            modItems.put(location.getNamespace(), items);
+        }
+        items.add(registered);
     }
 }
