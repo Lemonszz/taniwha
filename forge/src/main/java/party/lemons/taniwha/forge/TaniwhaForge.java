06433427a218d332a788c3851cd34efad8cf7898
Sam <lemonverse@gmail.com>
2023-05-04 21:06:36 +1200

    use correct bus

--- forge/src/main/java/party/lemons/taniwha/forge/TaniwhaForge.java
+++ forge/src/main/java/party/lemons/taniwha/forge/TaniwhaForge.java
@@ -3,6 +3,7 @@ package party.lemons.taniwha.forge;
 import dev.architectury.platform.Platform;
 import dev.architectury.platform.forge.EventBuses;
 import dev.architectury.utils.Env;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,7 +19,7 @@ public class TaniwhaForge {
         EventBuses.registerModEventBus(TConstants.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Taniwha.init();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(TaniwhaForge::clientSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(TForgeEvents::onPlaceEvent);
+        MinecraftForge.EVENT_BUS.addListener(TForgeEvents::onPlaceEvent);
     }
 
     public static void clientSetup(FMLClientSetupEvent event)
