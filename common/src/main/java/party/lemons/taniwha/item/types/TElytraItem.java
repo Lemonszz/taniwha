40d2a5ae8b237099e0e5dddc3b3cd9de267fdd20
Sam <lemonverse@gmail.com>
2023-06-14 20:38:44 +1200

    Elytra item improvements

--- common/src/main/java/party/lemons/taniwha/item/types/TElytraItem.java
+++ common/src/main/java/party/lemons/taniwha/item/types/TElytraItem.java
@@ -1,20 +1,39 @@
 package party.lemons.taniwha.item.types;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.Equipable;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import party.lemons.taniwha.item.TElytra;
 
 public class TElytraItem extends TItem implements TElytra, Equipable
 {
-	public TElytraItem(Properties properties)
+	@Nullable private final TagKey<Item> repairTag;
+
+	public TElytraItem(Properties properties, @Nullable TagKey<Item> repairTag)
 	{
 		super(properties);
+		this.repairTag = repairTag;
 		DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
+	}
+	@Override
+	public boolean isValidRepairItem(ItemStack arg, ItemStack arg2) {
+	return repairTag != null && arg2.is(repairTag);
+}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level arg, Player arg2, InteractionHand arg3) {
+		return this.swapWithEquipmentSlot(this, arg, arg2, arg3);
 	}
 
 	@Override
@@ -31,13 +50,18 @@ public class TElytraItem extends TItem implements TElytra, Equipable
 		}
 	}
 
+	@Override
+	public @NotNull SoundEvent getEquipSound() {
+		return SoundEvents.ARMOR_EQUIP_ELYTRA;
+	}
+
 	public boolean isRocketBoostEnabled(ItemStack stack)
 	{
 		return true;
 	}
 
 	@Override
-	public EquipmentSlot getEquipmentSlot() {
+	public @NotNull EquipmentSlot getEquipmentSlot() {
 		return EquipmentSlot.CHEST;
 	}
 }
