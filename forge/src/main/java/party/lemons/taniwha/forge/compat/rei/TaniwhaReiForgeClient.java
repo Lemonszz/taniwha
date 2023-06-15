1779888a18f2eeecf958a6ef32a0472d63f12b01
Sam <lemonverse@gmail.com>
2023-06-15 16:18:05 +1200

    Work?

---
+++ forge/src/main/java/party/lemons/taniwha/forge/compat/rei/TaniwhaReiForgeClient.java
@@ -0,0 +1,8 @@
+package party.lemons.taniwha.forge.compat.rei;
+
+import party.lemons.taniwha.compat.rei.TaniwhaReiClient;
+
+public class TaniwhaReiForgeClient extends TaniwhaReiClient
+{
+    //TODO: when rei is fixed
+}
