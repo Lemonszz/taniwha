3e826781c7c22fa9e073af9c51ce944604111099
Sam <lemonverse@gmail.com>
2023-07-16 12:21:15 +1200

    Fix stem blocks not recognising taniwha farmland

---
+++ common/src/main/java/party/lemons/taniwha/mixin/block/StemBlockMixin.java
@@ -0,0 +1,23 @@
+package party.lemons.taniwha.mixin.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.StemBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import party.lemons.taniwha.util.TaniwhaTags;
+
+@Mixin(StemBlock.class)
+public class StemBlockMixin
+{
+    @Inject(at = @At("HEAD"), method = "mayPlaceOn", cancellable = true)
+    private void mayPlaceOn(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Boolean> cbi)
+    {
+        if(blockState.is(TaniwhaTags.FARMLAND))
+            cbi.setReturnValue(true);
+    }
+}
